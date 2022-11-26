a0946240d2a96e29f10726b6508aba4b36f36616
NotTylee <Markodurden2004@gmail.com>
2022-11-26 01:11:08 -0500

    yes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+   
+    public float health = 50f;
+
+   
+
+  
+
+   
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+   
+}
